c14f6397d7013a26e969ed0d5fa9dd05d2b2d288
Niklas Tom Hucke <nitohu@gmail.com>
2022-07-28 22:17:21 +0200

    [ADD] basic password authentication for notes

--- better_notes/controllers/main.py
+++ better_notes/controllers/main.py
@@ -1,11 +1,25 @@
 from odoo import http
 from odoo.http import request
+import hashlib
 import logging
 
 _logger = logging.getLogger(__name__)
 
 
 class ShareNotes(http.Controller):
+
+    def _build_data(self, note):
+        return {
+            "content": note.memo,
+            "is_secured": note.is_secured,
+            "authorized": False
+        }
+
+    def _authorize(self, passwd, note):
+        h = hashlib.sha256(passwd.encode()).hexdigest()
+        if h == note.password:
+            return True
+        return False
 
     @http.route("/note/<string:note_id>", type="http", auth="public", website=True)
     def share_note(self, note_id=None):
@@ -15,9 +29,13 @@ class ShareNotes(http.Controller):
                 ("public", "=", True)
             ], limit=1)
             if note.id:
-                return request.render("better_notes.public_note", {
-                    "content": note.memo
-                })
+                data = self._build_data(note)
+                if request.httprequest.method == "POST":
+                    passwd = request.httprequest.form.get("password", False)
+                    # Authorize
+                    if passwd and self._authorize(passwd, note):
+                        data["authorized"] = True
+                return request.render("better_notes.public_note", data)
         return request.render("better_notes.public_note", {
             "content": "<h1>No note found!</h1>"
         })
--- better_notes/models/note_note.py
+++ better_notes/models/note_note.py
@@ -1,5 +1,8 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 import hashlib
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class NoteNote(models.Model):
@@ -19,10 +22,36 @@ class NoteNote(models.Model):
         string="Share note publically",
         default=False
     )
+    is_secured = fields.Boolean(
+        string="Secure with password",
+        help="Secure your public note with a password.",
+        default=False
+    )
+    password = fields.Char()
+    new_password = fields.Char(
+        compute="_reset_new_password",
+        inverse="_encrypt_password"
+    )
 
-    _sql_constraints = [
-        ("identifier_unique", "unique(identifier)", "This identifier already exists.")
-    ]
+    # FIXME: Throws error on fresh installation
+    # duplicate key value violates unique constraint "note_note_identifier_unique"
+    # DETAIL:  Key (identifier)=() already exists.
+    # _sql_constraints = [
+    #     ("identifier_unique", "unique(identifier)", "This identifier already exists.")
+    # ]
+
+    @api.model
+    def create(self, vals):
+        if vals["is_secured"]:
+            if not vals["new_password"] or vals["new_password"].strip() == "":
+                raise UserError(_("Please enter a password."))
+        return super().create(vals)
+
+    def write(self, vals):
+        if "is_secured" in vals and vals["is_secured"]:
+            if "new_password" not in vals or vals["new_password"].strip() == "":
+                raise UserError(_("Please enter a password."))
+        return super().write(vals)
 
     @api.depends("public")
     def _compute_public_identifier(self):
@@ -37,3 +66,14 @@ class NoteNote(models.Model):
             else:
                 r.identifier = ""
                 r.href = ""
+
+    def _reset_new_password(self):
+        for r in self:
+            r.new_password = ""
+
+    @api.depends("new_password")
+    def _encrypt_password(self):
+        for r in self:
+            if r.new_password:
+                r.password = hashlib.sha256(r.new_password.encode()).hexdigest()
+                r.new_password = ""
